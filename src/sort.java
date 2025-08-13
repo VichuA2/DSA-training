d0b77cf99fea694a35b3e81d97c17164d4b829be
V i s h n u <vivi420vishnu@gmail.com>
2025-08-13 12:53:12 +0530

    Update

---
+++ src/sort.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    Node previous;
+    Node (int data){
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
+}
+class LinkedList{
+    Node Head
+}
